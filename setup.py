94bb3d9fc537350a1a057a069d6b0e004e93c9a8
nlambriICH <nicola.lambri@cancercenter.humanitas.it>
2023-04-01 21:55:32 +0200

    Add setup.py to install the project as a package

---
+++ setup.py
@@ -0,0 +1,5 @@
+"""Setup script to install the project as package and import modules"""
+
+from setuptools import setup, find_packages
+
+setup(name="tmi_isocenter", version="0.0.1", packages=find_packages())
