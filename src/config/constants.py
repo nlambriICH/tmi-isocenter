6ca5df8a97d2943a0475b10c1313054b5af6e336
nlambriICH <nicola.lambri@cancercenter.humanitas.it>
2024-01-23 15:35:28 +0100

    Separate ptv mapping based on coll angle

--- src/config/constants.py
+++ src/config/constants.py
@@ -9,8 +9,6 @@ with open(constants_json_path) as file:
     data = json.load(file)
 
 PTV_EXCLUDE_SUBSTRINGS: list[str] = data["ptv_exclude_substrings"]
-MAP_ID_PTV: dict[str, str] = data["map_id_ptv"]
-MAP_ID_JUNCTION: dict[str, None | str | list[str]] = data["map_id_junction"]
 DICOM_PATH: str = data["dicom_path"]
 DICOM_DIR_TEST_PATH: str = data["dicom_dir_test_path"]
 MAP_ID_LUNGS: dict[str, list[str]] = data["map_lung_roi_name"]
@@ -20,7 +18,14 @@ COLL_5_355: bool = data["coll_5_355"]
 project_dir_path = dirname(dirname(config_dir_path))
 if COLL_5_355:
     dir_path = join(project_dir_path, "data", "5_355")
+    map_id_ptv = "map_id_ptv_coll_5_355"
+    map_id_junction = "map_id_junction_coll_5_355"
 else:
     dir_path = join(project_dir_path, "data", "90")
+    map_id_ptv = "map_id_ptv_coll_90"
+    map_id_junction = "map_id_junction_coll_90"
+
+MAP_ID_PTV: dict[str, str] = data[map_id_ptv]
+MAP_ID_JUNCTION: dict[str, None | str | list[str]] = data[map_id_junction]
 RAW_DATA_DIR_PATH: str = join(dir_path, "raw")
 INTERIM_DATA_DIR_PATH: str = join(dir_path, "interim")
